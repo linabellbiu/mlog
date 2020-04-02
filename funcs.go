7f007a3c9c14c602879925633318e721c82cd3ea
xudong <834971685@qq.com>
2020-04-02 10:37:46 +0800

    修改不能创建目录bug 修改不能输出当前执行的文件bug

--- funcs.go
+++ funcs.go
@@ -1,6 +1,8 @@
 package mlog
 
-import "os"
+import (
+	"os"
+)
 
 func cFile(filename string) *os.File {
 	file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
@@ -8,4 +10,8 @@ func cFile(filename string) *os.File {
 		panic("打开" + filename + "日志文件失败:" + err.Error())
 	}
 	return file
+}
+
+func cDir(path string) error {
+	return os.MkdirAll(path, os.ModePerm)
 }
--- new.go
+++ new.go
@@ -7,15 +7,18 @@ import (
 )
 
 const infoFileName = "info.log"
-const warningFileName = "warning.log"
 const errorFileName = "error.log"
+const warningFileName = "warning.log"
 
 var m = &minLog{
 	logLv: nil,
 }
 
 func New(filePath string) *minLog {
-	m.path = filePath
+	m.parse(filePath)
+	if err := cDir(m.path); err != nil {
+		panic("创建目录" + m.path + "失败")
+	}
 	m.logLv = m.newLogLv()
 	return m
 }
@@ -25,5 +28,13 @@ func (m *minLog) newLogLv() *logLv {
 		Info:    log.New(io.MultiWriter(os.Stderr, cFile(m.path+infoFileName)), "[INFO]: ", log.Ldate|log.Ltime|log.Lshortfile),
 		Warning: log.New(io.MultiWriter(os.Stderr, cFile(m.path+warningFileName)), "[WARNING]: ", log.Ldate|log.Ltime|log.Llongfile),
 		Error:   log.New(io.MultiWriter(os.Stderr, cFile(m.path+errorFileName)), "[ERROR]: ", log.Ldate|log.Ltime|log.Lmicroseconds|log.Llongfile),
+	}
+}
+
+func (m *minLog) parse(path string) {
+	m.path = path
+	l := len(path)
+	if string(path[l-1]) != "/" {
+		m.path = path + "/"
 	}
 }
