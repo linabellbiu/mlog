6857f9f624bfeadc521f44094c055699ad292c45
xudong <834971685@qq.com>
2020-06-08 20:19:57 +0800

    修改输出信息

--- options.go
+++ options.go
@@ -1,13 +1,28 @@
 package mlog
 
+import "os"
+
 func (m *minLog) Info(msg string) {
 	m.logLv.Info.Output(2, msg)
 }
 
-func (m *minLog) Warning(msg string) {
-	m.logLv.Warning.Output(2, msg)
+func (m *minLog) Warning(sign string, err error, msg string) {
+	var error string
+	if err != nil {
+		error = err.Error()
+	} else {
+		error = ""
+	}
+	m.logLv.Warning.Output(2, "\n[Type]:"+sign+"\n[Error]:"+error+"\n[Msg]:"+msg)
 }
 
-func (m *minLog) Error(msg string) {
-	m.logLv.Error.Output(2, msg)
+func (m *minLog) Error(sign string, err error) {
+	var error string
+	if err != nil {
+		error = err.Error()
+	} else {
+		error = ""
+	}
+	m.logLv.Error.Output(2, "\n[Type]:"+sign+"\n[Error]:"+error)
+	os.Exit(1)
 }
