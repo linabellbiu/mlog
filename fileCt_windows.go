78d41a016592bcf6c20096b4c808f4b96cf18ee9
xudong <834971685@qq.com>
2020-09-24 10:41:44 +0800

    修改windows下编译出错

--- fileCt_windows.go
+++ fileCt_windows.go
@@ -7,10 +7,10 @@ import (
 	"time"
 )
 
-func getCreateTime(path string) {
+func getCreateTime(path string) int64 {
 	osType := runtime.GOOS
 	fileInfo, _ := os.Stat(path)
-	if ostosTypeype == "windows" {
+	if osType == "windows" {
 		//Win下文件创建时间
 		fileSys := fileInfo.Sys().(*syscall.Win32FileAttributeData)
 		nanoseconds := fileSys.CreationTime.Nanoseconds() // 返回的是纳秒
